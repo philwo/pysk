3f4f921723c77d1b554823acc7cc59664f3d6710
root <root@mikuru.igowo.de>
2009-09-05 21:34:28 +0200

    added new monit scripts

--- serverconfig/copy.sh
+++ serverconfig/copy.sh
@@ -4,13 +4,19 @@ set -e
 set -u
 
 EXCLUDEFILE="/opt/pysk/etc/serverconfig/exclude/`hostname`"
-RSYNCOPTS="-a --exclude copy.sh"
+RSYNCOPTS="-a --exclude copy.sh --exclude diff.sh"
+MAINUSER=`grep "igowo user" /etc/passwd | head -n1 | cut -d":" -f1`
 
 if [ -s $EXCLUDEFILE ] ; then
 	RSYNCOPTS="$RSYNCOPTS --exclude-from=$EXCLUDEFILE"
 fi
 
 rsync $RSYNCOPTS /opt/pysk/serverconfig/ /
+
+echo "Fixing permissions"
 chmod 0700 /root /root/.ssh /etc/monit.d
 chmod 0600 /etc/monitrc /root/.pgpass /root/.ssh/*
+
+echo "Fixing up httpd.conf"
+sed -i s/MAINUSER/$MAINUSER/g /etc/httpd/conf/httpd.conf
 
