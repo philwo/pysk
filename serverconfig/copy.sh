7a6c7d58549d097323537c0ce2b815b62dbc33de
root <root@kyon.igowo.de>
2009-09-05 13:00:54 +0200

    Finalized host-based exclude list for server configs

--- serverconfig/copy.sh
+++ serverconfig/copy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-rsync -a --exclude=copy.sh /opt/pysk/serverconfig/ /
+set -e
+set -u
+
+EXCLUDEFILE="/opt/pysk/etc/serverconfig/exclude/`hostname`"
+RSYNCOPTS="-a --exclude copy.sh"
+
+if [ -s $EXCLUDEFILE ] ; then
+	RSYNCOPTS="$RSYNCOPTS --exclude-from=$EXCLUDEFILE"
+fi
+
+rsync $RSYNCOPTS /opt/pysk/serverconfig/ /
 chmod 0700 /root /root/.ssh /etc/monit.d
 chmod 0600 /etc/monitrc /root/.pgpass /root/.ssh/*
 
