7c7db85e3d77d1a4d53038f7ca23198aceecd006
root <root@haruhi.igowo.de>
2009-09-10 03:26:50 +0200

    NARF

--- tools/mysql/setup.sh
+++ tools/mysql/setup.sh
@@ -4,6 +4,18 @@ set -e
 set -u
 set -x
 
+# Remove anonymous users
+mysql -e "DELETE FROM mysql.user WHERE User='';"
+
+# Drop test database
+mysql -e "DROP DATABASE test;"
+
+# Remove privileges from test database
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+
+# Reload privileges tables
+mysql -e "FLUSH PRIVILEGES;"
+
 # Generate random password for MySQL
 rootpw=`tr -cd '[:alnum:]' < /dev/urandom | head -c 12`
 
@@ -22,16 +34,4 @@ password = $rootpw
 host = localhost
 OMG
 chmod 0600 /root/.my.cnf
-
-# Remove anonymous users
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-
-# Drop test database
-mysql -e "DROP DATABASE test;"
-
-# Remove privileges from test database
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-
-# Reload privileges tables
-mysql -e "FLUSH PRIVILEGES;"
 
