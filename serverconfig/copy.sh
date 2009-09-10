fa56e3f759981768df21df239791b857c34fbe1c
root <root@c-ds.igowo.de>
2009-09-10 22:33:41 +0200

    ...

--- serverconfig/copy.sh
+++ serverconfig/copy.sh
@@ -24,5 +24,5 @@ chmod 0600 /etc/ssl/private/*
 
 echo "Fixing up httpd.conf"
 sed -i s/MAINUSER/$MAINUSER/g /etc/httpd/conf/httpd.conf
-sed -i s/MAINUSER/$MAINUSER/g /etc/php/php-fpm.conf
+sed -i s/XXXMAINUSERXXX/$MAINUSER/g /etc/php/php-fpm.conf
 
