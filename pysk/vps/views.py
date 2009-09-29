c5afa88e0e21613a19b11db98b95439bde475604
root <root@c-ps1-1.igowo.de>
2009-09-29 22:48:50 +0200

    fix fastcgi handler naming

--- pysk/vps/views.py
+++ pysk/vps/views.py
@@ -314,10 +314,10 @@ def v0_apache(request, server):
                         output.append("RPAFproxy_ips 127.0.0.1")
 
                     # PHP via FastCGI
-                    output.append("FastCGIExternalServer /home/%s/www/%s/htdocs/fast-cgi-fake-handler -host 127.0.0.1:9000" % (username, vh.fqdn()))
+                    output.append("FastCGIExternalServer /home/%s/www/%s/htdocs/fast-cgi-fake-handler-%s-%s -host 127.0.0.1:9000" % (username, vh.fqdn(), ip.ip.replace(".", "-"), ip.port))
                     output.append("<Files ~ \"\\.php$\">")
                     output.append("    AddType application/x-httpd-fastphp5 .php")
-                    output.append("    Action application/x-httpd-fastphp5 /fast-cgi-fake-handler")
+                    output.append("    Action application/x-httpd-fastphp5 /fast-cgi-fake-handler-%s-%s" % (ip.ip.replace(".", "-"), ip.port))
                     output.append("</Files>")
 
                     output.append("</VirtualHost>")
