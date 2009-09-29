bd767bf473d2fc94d5dabe959fa08c6b5c68df6a
root <root@haruhi.igowo.de>
2009-09-30 01:52:06 +0200

    ...

--- tools/web.sh
+++ tools/web.sh
@@ -3,6 +3,9 @@
 set -e
 set -u
 
+/opt/pysk/tools/dovecot/dovecot.sh
+/opt/pysk/tools/postfix/postfix.sh
+
 /opt/pysk/tools/apache/apache.py
 /opt/pysk/tools/nginx/nginx.py
 /opt/pysk/tools/passwd/passwd.py
