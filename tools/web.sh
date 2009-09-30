05f66d3bc62fe1e5811a5f96543f3bd44cb137e5
root <root@c-chris-1.igowo.de>
2009-09-30 06:01:50 +0200

    ...

--- tools/web.sh
+++ tools/web.sh
@@ -3,8 +3,8 @@
 set -e
 set -u
 
-/opt/pysk/tools/dovecot/dovecot.sh
-/opt/pysk/tools/postfix/postfix.sh
+/opt/pysk/tools/dovecot/dovecot.sh || /bin/true
+/opt/pysk/tools/postfix/postfix.sh || /bin/true
 
 /opt/pysk/tools/apache/apache.py
 /opt/pysk/tools/nginx/nginx.py
