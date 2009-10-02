e40e9001a70c9c24edafc9166c46f90990903483
root <root@haruhi.igowo.de>
2009-10-02 12:18:25 +0200

    updated replace-vi-with-vim snippet

--- snippets/replace_vi_with_vim.sh
+++ snippets/replace_vi_with_vim.sh
@@ -5,6 +5,7 @@ set -u
 
 pacman -Rcs --noconfirm vi
 rm -f /usr/bin/rview
-pacman -S --noconfirm vim
+pacman -S --noconfirm --force vim
 ln -s /usr/bin/vim /usr/local/bin/vi
+cp /opt/pysk/serverconfig/etc/vimrc /etc
 
