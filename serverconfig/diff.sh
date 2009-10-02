3e2525af08432bd0b887bb8d7765acbdc020a7b1
root <root@haruhi.igowo.de>
2009-10-02 10:30:57 +0200

    ...

--- serverconfig/diff.sh
+++ serverconfig/diff.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-diff -ur -x php-fpm.conf /opt/pysk/serverconfig/ / | grep -v "Only in /" | less
+EXCLUDEFILE="/opt/pysk/etc/serverconfig/exclude/`hostname`"
+EXCLUDES=""
+
+if [ -s $EXCLUDEFILE ] ; then
+    for i in `cat $EXCLUDEFILE`; do
+        $EXCLUDES="$EXCLUDES -x $i"
+    done
+fi
+
+echo "diff -ur $EXCLUDES /opt/pysk/serverconfig/ / | grep -v 'Only in /' | less"
 
