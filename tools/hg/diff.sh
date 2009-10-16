8fb7307f66f5a95f24babe13a1c0ef72b37f0006
Philipp Wollermann <philipp@igowo.de>
2009-10-16 13:43:55 +0200

    removed hg sync scripts again, new version are on haruhi

--- tools/hg/diff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ssh root@haruhi "cd /opt/pysk && hg diff"
-ssh root@mikuru "cd /opt/pysk && hg diff"
-ssh root@yuki "cd /opt/pysk && hg diff"
-ssh root@kyon "cd /opt/pysk && hg diff"
-cd ~/src/pysk && hg diff
-
