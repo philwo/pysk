5c3e099786447aa8059ca84e1aca52b59882444d
root <root@haruhi.igowo.de>
2009-10-25 16:36:30 +0100

    added hosts.yml, changed replace vi with vim snippet

--- snippets/replace_vi_with_vim.sh
+++ snippets/replace_vi_with_vim.sh
@@ -6,6 +6,6 @@ set -u
 pacman -Rcs --noconfirm vi
 rm -f /usr/bin/rview
 pacman -S --noconfirm --force vim
-ln -s /usr/bin/vim /usr/local/bin/vi
+ln -s /usr/bin/vim /usr/bin/vi
 cp /opt/pysk/serverconfig/etc/vimrc /etc
 
