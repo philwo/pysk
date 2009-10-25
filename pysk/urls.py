f174180958fbb915734db1144869c847f22ae81f
Philipp Wollermann <philipp@igowo.de>
2009-10-26 00:37:50 +0100

    ...

--- pysk/urls.py
+++ pysk/urls.py
@@ -23,14 +23,15 @@ urlpatterns = patterns("",
 
 # Muss per HTTP Auth abgesichert werden!
 urlpatterns += patterns("pysk.vps.views",
-	(r'^api/v0/vz/apache/', 'v0_apache'),
-	(r'^api/v0/vz/aliases/', 'v0_aliases'),
-	(r'^api/v0/vz/aliases_nginx/', 'v0_aliases_nginx'),
-	(r'^api/v0/dns/bind/', 'bind'),
+	(r'^api/v0/vz/apache/$', 'v0_apache'),
+	(r'^api/v0/vz/aliases/$', 'v0_aliases'),
+	(r'^api/v0/vz/aliases_nginx/$', 'v0_aliases_nginx'),
+	(r'^api/v0/dns/bind/$', 'bind'),
 )
 
 urlpatterns += patterns("pysk.vps.views",
-    (r'^actions/save/', 'save'),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'myapp/login.html'}),
+    (r'^actions/save/$', 'save'),
 )
 
 urlpatterns += patterns('django.views.generic.simple',
