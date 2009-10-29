ad80b47a38bafc8c694d9216ab23c38bcfdadc93
root <root@c-simon-1.igowo.de>
2009-10-29 23:05:25 +0100

    tools/nginx: disable wget ssl certificate checking, because it doesn't support SNI :(

--- tools/nginx/run.sh
+++ tools/nginx/run.sh
@@ -9,7 +9,7 @@ APIPASS="W68p20YST5Iv6KGG"
 /opt/pysk/tools/nginx/nginx.py
 
 # Aliases
-wget --user=pysk --password=$APIPASS -O/etc/nginx/conf/sites-available/aliases https://`hostname`/api/v0/vz/aliases_nginx/ 2>/dev/null
+wget --user=pysk --password=$APIPASS --no-check-certificate -O/etc/nginx/conf/sites-available/aliases https://`hostname`/api/v0/vz/aliases_nginx/ 2>/dev/null
 ln -s /etc/nginx/conf/sites-available/aliases /etc/nginx/conf/sites-enabled/aliases
 
 #/etc/rc.d/nginx reload
