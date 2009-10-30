8d1b76a7f9b9edec745042ef7c4e40304b9cf07a
root <root@mikuru.igowo.de>
2009-10-30 12:21:53 +0100

    it usually makes sense to first change the schema, then use the new column. ouch. -.-

--- snippets/20091027_pysk_add_virtualhost_sortkey.sh
+++ snippets/20091027_pysk_add_virtualhost_sortkey.sh
@@ -4,6 +4,6 @@ set -e
 set -u
 
 cd /opt/pysk/pysk
-echo 'for vh in VirtualHost.objects.all(): vh.save()' | python manage.py shell_plus
 echo 'ALTER TABLE "vps_virtualhost" ADD "sortkey" varchar(255);' | psql pysk
+echo 'for vh in VirtualHost.objects.all(): vh.save()' | python manage.py shell_plus
 
