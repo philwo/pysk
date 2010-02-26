29bb44e2ceaaadae3aed305bb3e0f42f84687e4c
Philipp Wollermann <philipp@igowo.de>
2010-02-27 00:18:52 +0100

    tools/dns: Improved SOA serial generator

--- tools/dns/serial.py
+++ tools/dns/serial.py
@@ -14,44 +14,66 @@ from glob import glob
 import hashlib
 import pprint
 
-t = date.today().strftime("%Y%m%d")
+opts = None
 
-zones = {}
-if os.path.exists("/var/named/serial"):
-    zones = pickle.load(open("/var/named/serial", "r"))
+def update():
+    t = date.today().strftime("%Y%m%d")
+    
+    for zonefile in glob("/var/named/pysk/db.*"):
+        zonename = os.path.basename(zonefile)[3:]
 
-def update(self):
-for zonefile in glob("/var/named/pysk/db.*"):
-    zonename = os.path.basename(zonefile)[3:]
+        if not zonename in zones:
+            zones[zonename] = {}
+            zones[zonename]["hash"] = ""
+            zones[zonename]["serial"] = 0
 
-    if not zonename in zones:
-        zones[zonename] = {}
-        zones[zonename]["hash"] = ""
-        zones[zonename]["serial"] = 0
+        newhash = hashlib.sha512("".join(open(zonefile, "r").readlines())).hexdigest()
 
-    newhash = hashlib.sha512("".join(open(zonefile, "r").readlines())).hexdigest()
+        if newhash != zones[zonename]["hash"]:
+            # Update stored hash of zonefile
+            zones[zonename]["hash"] = newhash
+            oldserial = zones[zonename]["serial"]
 
-    if newhash != zones[zonename]["hash"]:
-        # Update stored hash of zonefile
-        zones[zonename]["hash"] = newhash
-        oldserial = zones[zonename]["serial"]
-
-        # Do we already have a serial?
-        if zones[zonename]["serial"] == 0:
-            # No
-            c = 1
-        else:
-            # Yes, but is it from today?
-            d = str(zones[zonename]["serial"])[:8]
-            if (t != d):
+            # Do we already have a serial?
+            if zones[zonename]["serial"] == 0:
                 # No
                 c = 1
             else:
-                # Yes
-                c = int(str(zones[zonename]["serial"])[8:]) + 1
-        zones[zonename]["serial"] = "%s%02i" % (t, c)
-        print >>sys.stderr, "%s: Hash mismatch, updated serial from %s to %s" % (zonename, oldserial, zones[zonename]["serial"],)
+                # Yes, but is it from today?
+                d = str(zones[zonename]["serial"])[:8]
+                if (t != d):
+                    # No
+                    c = 1
+                else:
+                    # Yes
+                    c = int(str(zones[zonename]["serial"])[8:]) + 1
+            
+            zones[zonename]["serial"] = "%s%02i" % (t, c)
+            
+            if opts.verbose:
+                print >>sys.stderr, "%s: Hash mismatch, updated serial from %s to %s" % (zonename, oldserial, zones[zonename]["serial"],)
+    
+    pickle.dump(zones, open("/var/named/serial", "w"))
 
-#pprint.PrettyPrinter(indent=4).pprint(zones)
-pickle.dump(zones, open("/var/named/serial", "w"))
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
+    zones = {}
+    if os.path.exists("/var/named/serial"):
+        zones = pickle.load(open("/var/named/serial", "r"))
+
+    usage = "usage: %s [--update] [zone]" % (argv[0])
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-u", "--update", action="store_true", dest="update", default=False)
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+    parser.add_option("-q", "--quiet", action="store_false", dest="verbose")
+    (opts, args) = parser.parse_args(argv)
+    
+    if opts.update:
+        update(zones)
+    
+    print args
+    
+if __name__ == "__main__":
+    sys.exit(main())
