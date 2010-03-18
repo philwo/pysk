b731080a35840769091b27a6a962085a82658a56
Philipp Wollermann <philipp@igowo.de>
2010-03-18 23:19:44 +0100

    duplicity.sh improved

--- tools/backup/duplicity.sh
+++ tools/backup/duplicity.sh
@@ -3,11 +3,15 @@
 set -e
 set -u
 
+FTP_HOST="88.198.42.117"
+FTP_USER="48554"
 FTP_PASSWORD="vzOQuQBd"
-FTP_URL="ftp://48554@88.198.42.117/duplicity/`hostname`/"
+FTP_URL="ftp://$FTP_USER@$FTP_HOST/duplicity/`hostname`/"
 PASSPHRASE="cY1V78sCkxeZ7jpReOCyixH8"
 
 set -x
+
+lftp -c "open $FTP_HOST && login $FTP_USER $FTP_PASSWORD && cd duplicity && mkdir `hostname`" &> /dev/null || /bin/true
 
 FTP_PASSWORD=$FTP_PASSWORD PASSPHRASE=$PASSPHRASE duplicity --full-if-older-than 6D --encrypt-key "FA3B07B8" --sign-key "FA3B07B8" -v8 \
     --gpg-options='--compress-algo=bzip2 --bzip2-compress-level=9' --asynchronous-upload \
